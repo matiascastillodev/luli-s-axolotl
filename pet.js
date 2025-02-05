6ae72350d752188e48bd1ff1257bca1af1fc856c
Matias Castillo <177944762+matiascastillodev@users.noreply.github.com>
2025-02-05 12:24:43 -0300

    moving non method functions from pet to script

--- pet.js
+++ pet.js
@@ -4,52 +4,28 @@ class Pet {
   constructor(name) {
     this.name = name;
     this.hunger = 7;
+    this.maxHunger = 7;
     this.health = 7;
+    this.maxHealth = 7;
     this.energy = 5;
+    this.maxEnergy = 7;
     this.isSleeping = false;
     this.healthIncreaseInterval = null;
-    this.gameOver = false;
-
-    this.startGameLoop();
   }
-
-  startGameLoop() {
-    setInterval(() => {
-      this.hunger = Math.max(0, this.hunger - 1);
-      updateStats();
-    }, 14000);
-
-    setInterval(() => {
-      if (!this.isSleeping) {
-        this.energy = Math.max(0, this.energy - 1);
-        this.checkEnergy();
-        updateStats();
-      }
-    }, 24000);
-
-    setInterval(() => {
-      this.checkHealth();
-    }, 1000);
-  }
-
   checkHealth() {
     if (
       this.hunger === 0 ||
       (this.energy === 0 && !this.isSleeping) ||
-      this.energy === 7
+      this.energy === this.maxEnergy
     ) {
       this.health = Math.max(0, this.health - 1);
       updateStats();
       if (this.health === 0) {
-        this.gameOver = true;
-        clearInterval(this.hungerInterval);
-        clearInterval(this.energyInterval);
-        clearInterval(this.healthInterval);
         gameOver();
       }
     }
 
-    if (this.hunger === 7 && this.energy > 0) {
+    if (this.hunger === this.maxHunger && this.energy > 0) {
       if (!this.healthIncreaseInterval) {
         this.healthIncreaseInterval = setInterval(() => {
           if (this.health < 7) {
@@ -69,31 +45,11 @@ class Pet {
     }
   }
 
-  checkEnergy() {
-    if (this.energy === 0) {
-      console.log("Pet needs to sleep.");
-    } else if (this.energy === 7) {
-      console.log("Pet needs to play.");
-    }
-  }
-
   feed() {
     if (this.isSleeping) {
-      console.log("Pet is sleeping.");
-      return;
-    } else if (!this.isSleeping) {
-      this.hunger = Math.min(7, this.hunger + 1);
-      updateStats();
-    }
-  }
-
-  play() {
-    console.log(this.isSleeping);
-    if (this.isSleeping) {
-      console.log("Pet is sleeping.");
       return;
     } else {
-      this.energy = Math.max(0, this.energy - 1);
+      this.hunger = Math.min(7, this.hunger + 1);
       updateStats();
     }
   }
@@ -102,21 +58,27 @@ class Pet {
     if (this.isSleeping) {
       this.isSleeping = false;
       clearInterval(this.sleepInterval);
-      console.log("Pet has woken up.");
     } else {
       this.isSleeping = true;
       console.log("Pet is sleeping.");
       this.sleepInterval = setInterval(() => {
-        if (this.energy < 7) {
+        if (this.energy < this.maxEnergy) {
           this.energy++;
-          this.checkEnergy();
           updateStats();
         } else {
           clearInterval(this.sleepInterval);
-          console.log("Pet is fully rested.");
           updateStats();
         }
       }, 1000);
+    }
+  }
+
+  play() {
+    if (this.isSleeping) {
+      return;
+    } else {
+      this.energy = Math.max(0, this.energy - 1);
+      updateStats();
     }
   }
 }
--- script.js
+++ script.js
@@ -1,47 +1,63 @@
 import { Pet } from "./pet.js";
 
 let mainImg = document.querySelector(".pet");
-
 let pet;
-const feedBtn = document.getElementById("feedBtn");
-const playBtn = document.getElementById("playBtn");
-const sleepBtn = document.getElementById("sleepBtn");
 
-feedBtn.addEventListener("click", () => pet.feed());
-playBtn.addEventListener("click", () => pet.play());
-sleepBtn.addEventListener("click", () => {
-  pet.sleep();
-  toggleSleepIcon();
-  updateButtonsState();
-});
-
-function toggleSleepIcon() {
-  let sleepIcon = document.getElementById("sleepIcon");
-  let statusText = document.getElementById("statusText");
-  let currentSrc = sleepIcon.src;
-
-  let image1 = "images/Bucket_of_Axolotl_JE1_BE1.webp";
-  let image2 = "images/Water_Bucket_JE2_BE2.webp";
-
-  if (currentSrc.includes(image1)) {
-    sleepIcon.src = image2;
-    statusText.textContent = "SLEEP";
-    mainImg.src = "images/Axolotl_Walk_Floor_Underwater.webp";
-  } else {
-    sleepIcon.src = image1;
-    statusText.textContent = "WAKE";
-    mainImg.src = "images/Axolotl_Idle_Floor_Underwater.webp";
-  }
+function nameSet() {
+  const modal = document.querySelector("#gameStart");
+  modal.style.display = "block";
+  const setNameBtn = document.querySelector("#setNameBtn");
+  setNameBtn.addEventListener("click", () => {
+    const nameInput = document.querySelector("#nameInput").value;
+    if (nameInput) {
+      document.querySelector("#nameDisplay").textContent = nameInput;
+      modal.style.display = "none";
+      gameStart(nameInput);
+    }
+  });
 }
 
-function updateButtonsState() {
-  if (pet.isSleeping) {
-    feedBtn.disabled = true;
-    playBtn.disabled = true;
-  } else {
-    feedBtn.disabled = false;
-    playBtn.disabled = false;
-  }
+nameSet();
+
+function gameStart(petName) {
+  pet = new Pet(petName);
+
+  setInterval(() => {
+    pet.hunger = Math.max(0, pet.hunger - 1);
+    updateStats();
+  }, 14000);
+
+  setInterval(() => {
+    if (!pet.isSleeping) {
+      pet.energy = Math.max(0, pet.energy - 1);
+      pet.checkEnergy();
+      updateStats();
+    }
+  }, 24000);
+
+  setInterval(() => {
+    pet.checkHealth();
+  }, 1000);
+
+  updateStats();
+}
+
+function gameOver() {
+  const modal = document.getElementById("gameOver");
+  modal.style.display = "block";
+
+  const restartBtn = document.getElementById("restartBtn");
+  restartBtn.onclick = function () {
+    modal.style.display = "none";
+    resetGame();
+  };
+}
+
+function resetGame() {
+  pet.hunger = 7;
+  pet.health = 7;
+  pet.energy = 5;
+  nameSet();
 }
 
 function updateStats() {
@@ -71,44 +87,45 @@ function getStatImages(image, count) {
   return Array(count).fill(`<img src="images/${image}" alt="">`).join("");
 }
 
-function gameOver() {
-  const modal = document.getElementById("gameOver");
-  modal.style.display = "block";
+const feedBtn = document.getElementById("feedBtn");
+const playBtn = document.getElementById("playBtn");
+const sleepBtn = document.getElementById("sleepBtn");
 
-  const restartBtn = document.getElementById("restartBtn");
-  restartBtn.onclick = function () {
-    modal.style.display = "none";
-    resetGame();
-  };
+feedBtn.addEventListener("click", () => pet.feed());
+playBtn.addEventListener("click", () => pet.play());
+sleepBtn.addEventListener("click", () => {
+  pet.sleep();
+  toggleSleepIcon();
+  disableActionsWhenAsleep();
+});
+
+function toggleSleepIcon() {
+  let sleepIcon = document.getElementById("sleepIcon");
+  let statusText = document.getElementById("statusText");
+  let currentSrc = sleepIcon.src;
+
+  let image1 = "images/Bucket_of_Axolotl_JE1_BE1.webp";
+  let image2 = "images/Water_Bucket_JE2_BE2.webp";
+
+  if (currentSrc.includes(image1)) {
+    sleepIcon.src = image2;
+    statusText.textContent = "SLEEP";
+    mainImg.src = "images/Axolotl_Walk_Floor_Underwater.webp";
+  } else {
+    sleepIcon.src = image1;
+    statusText.textContent = "WAKE";
+    mainImg.src = "images/Axolotl_Idle_Floor_Underwater.webp";
+  }
 }
 
-function resetGame() {
-  pet.hunger = 7;
-  pet.health = 7;
-  pet.energy = 5;
-  nameSet();
+function disableActionsWhenAsleep() {
+  if (pet.isSleeping) {
+    feedBtn.disabled = true;
+    playBtn.disabled = true;
+  } else {
+    feedBtn.disabled = false;
+    playBtn.disabled = false;
+  }
 }
-
-function nameSet() {
-  const modal = document.getElementById("gameStart");
-  modal.style.display = "block";
-
-  const setNameBtn = document.getElementById("setNameBtn");
-  setNameBtn.onclick = function () {
-    const petNameInput = document.getElementById("petNameInput").value;
-    if (petNameInput) {
-      document.getElementById("petName").textContent = petNameInput;
-      modal.style.display = "none";
-      gameStart(petNameInput);
-    }
-  };
-}
-
-function gameStart(petName) {
-  pet = new Pet(petName);
-  updateStats();
-}
-
-nameSet();
 
 export { updateStats, gameOver };
